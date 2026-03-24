552d102fc5b0d517a113de5c3a020ac9c67bf192
Oliver Foskin <K00293081@student.tus.ie>
2026-03-24 11:52:06 +0000

    Edited before playtest

--- Assets/SCRIPTS/MK Scripts/EntryDialogue.cs
+++ Assets/SCRIPTS/MK Scripts/EntryDialogue.cs
@@ -7,7 +7,7 @@ public class EntryDialogue : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(KeepTextOn(7f));
+        StartCoroutine(KeepTextOn(4f));
     }
 
     IEnumerator KeepTextOn(float waitTime)
