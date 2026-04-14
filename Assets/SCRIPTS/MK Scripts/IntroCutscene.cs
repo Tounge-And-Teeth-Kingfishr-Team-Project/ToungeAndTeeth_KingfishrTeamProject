e670634445072e298e92d805d21c254ee9508a82
michael-karsa-tus23 <k00299378@student.tus.ie>
2026-04-14 16:25:31 +0100

    Separated cutscene from exterior scene

--- Assets/SCRIPTS/MK Scripts/IntroCutscene.cs
+++ Assets/SCRIPTS/MK Scripts/IntroCutscene.cs
@@ -3,26 +3,15 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 using static UnityEngine.UI.Image;
 
 public class IntroCutscene : MonoBehaviour
 {
-    public GameObject[] objectsToSwitch;
-    public GameObject player;
-    public Transform putPlayerHere;
-    //public GameObject virtualCamera;
     public PlayableDirector cutscene;
-    public ParticleSystem smoke;
-    public Pause_Script pauseScript;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //virtualCamera.SetActive(true);
-        foreach (GameObject g in objectsToSwitch)
-        {
-            g.SetActive(false);
-        }
-        //player.SetActive(false);
         cutscene.Play();
         StartCoroutine(waitForCutsceneFinish());
     }
@@ -30,7 +19,7 @@ public class IntroCutscene : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            cutscene.Stop();
+            LoadLevel();
         }
     }
     private IEnumerator waitForCutsceneFinish()
@@ -40,20 +29,10 @@ public class IntroCutscene : MonoBehaviour
         {
             yield return null;  // Keep checking the timeline state
         }
-        //virtualCamera.SetActive(false);
-        //player.SetActive(true);
-        if (smoke != null)
-        {
-            smoke.Play();
-        }
-        foreach (GameObject g in objectsToSwitch)
-        {
-            g.SetActive(true);
-        }
-        GameObject thePlayer;
-        thePlayer = Instantiate(player, putPlayerHere);
-        thePlayer.GetComponent<FlashlightController>().enabled = false;
-        // pauseScript.pauseActive = true;
-        Destroy(gameObject);
+        LoadLevel();
+    }
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
